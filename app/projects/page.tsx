438b05a3343e5085323176b7629f5ba9a57133a9
gabrieldemaj-tetbit <gabrieldemaj22@gmail.com>
2026-01-26 13:20:47 +0100

    added more projetcs

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,4 @@
 // app/projects/page.tsx
-import Link from "next/link";
 import React from "react";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
@@ -35,6 +34,24 @@ const allProjects: Project[] = [
     link: "https://apps.apple.com/al/app/klan-kosova/id1608890216",
   },
   {
+    slug: "fajnd-delivery-food-and-more",
+    title: "Fajnd (Mobile App)",
+    description:
+      "A food delivery app for ordering food and more from local restaurants and stores.",
+    date: "2026-01-26",
+    published: true,
+    link: "https://apps.apple.com/us/app/fajnd-delivery-food-and-more/id6741737180",
+  },
+  {
+    slug: "fajnd-partner",
+    title: "Fajnd Partner (Mobile App)",
+    description:
+      "A partner app for Fajnd, a food delivery app for ordering food and more from local restaurants and stores.",
+    date: "2026-01-26",
+    published: true,
+    link: "https://apps.apple.com/us/app/fajnd-partner/id6744256690",
+  },
+  {
     slug: "two-jobs-website",
     title: "Two Jobs (Website)",
     description:
@@ -53,12 +70,39 @@ const allProjects: Project[] = [
     link: "https://www.pro-rks.com/?page=1",
   },
   {
+    slug: "prishtina-real-estate",
+    title: "Prishtina Real Estate (Website)",
+    description:
+      "A modern real estate platform offering listings and property management tools.",
+    date: "2026-01-26",
+    published: true,
+    link: "https://prishtina.io/",
+  },
+  {
+    slug: "creaclean",
+    title: "Creaclean (Website)",
+    description:
+      "A cleaning company in Switzerland that offers cleaning services for homes and offices.",
+    date: "2026-01-26",
+    published: true,
+    link: "https://www.creaclean.ch/en",
+  },
+  {
     slug: "socialist",
     title: "SOCIALIST (Website)",
     description: "Swiss agency creating videos and ads to grow brands.",
     date: "2025-08-22",
     published: true,
     link: "https://www.socialist.ch/",
+  },
+  {
+    slug: "fajnd-web",
+    title: "Fajnd Web (Website)",
+    description:
+      "A web application for managing and tracking tasks and projects.",
+    date: "2025-08-22",
+    published: true,
+    link: "https://fajnd.app/",
   },
 ];
 
@@ -71,21 +115,8 @@ const views = allProjects.reduce((acc, p) => {
 export const revalidate = 60;
 
 export default function ProjectsPage() {
-  const featured = allProjects[0];
-  const top2 = allProjects[1];
-  const top3 = allProjects[2];
-  const sorted = allProjects
-    .filter((p) => p.published)
-    .filter(
-      (project) =>
-        project.slug !== featured.slug &&
-        project.slug !== top2.slug &&
-        project.slug !== top3.slug
-    )
-    .sort(
-      (a, b) =>
-        new Date(a.date as any).getTime() - new Date(b.date as any).getTime()
-    );
+  const mobileApps = allProjects.slice(0, 5);
+  const webProjects = allProjects.slice(5);
 
   return (
     <div className="relative pb-16">
@@ -101,84 +132,21 @@ export default function ProjectsPage() {
         </div>
         <div className="w-full h-px bg-zinc-800" />
 
-        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2">
-          <Card>
-            <Link
-              href={featured.link}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <article className="relative w-full h-full p-4 md:p-8">
-                <div className="flex items-center justify-between gap-2">
-                  <div className="text-xs text-zinc-100">
-                    {featured.date ? (
-                      <time dateTime={new Date(featured.date).toISOString()}>
-                        {Intl.DateTimeFormat(undefined, {
-                          dateStyle: "medium",
-                        }).format(new Date(featured.date))}
-                      </time>
-                    ) : (
-                      <span>SOON</span>
-                    )}
-                  </div>
-                </div>
-
-                <h2
-                  id="featured-post"
-                  className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
-                >
-                  {featured.title}
-                </h2>
-                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
-                  {featured.description}
-                </p>
-                <div className="absolute bottom-4 md:bottom-8">
-                  <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
-                    Read more <span aria-hidden="true">&rarr;</span>
-                  </p>
-                </div>
-              </article>
-            </Link>
-          </Card>
-
-          <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0">
-            {[top2, top3].map((project) => (
-              <Card key={project.slug}>
-                <Article project={project} views={views[project.slug] ?? 0} />
-              </Card>
-            ))}
-          </div>
+        <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-2 lg:grid-cols-3">
+          {mobileApps.map((project) => (
+            <Card key={project.slug}>
+              <Article project={project} views={views[project.slug] ?? 0} />
+            </Card>
+          ))}
         </div>
         <div className="hidden w-full h-px md:block bg-zinc-800" />
 
-        <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
-          <div className="grid grid-cols-1 gap-4">
-            {sorted
-              .filter((_, i) => i % 3 === 0)
-              .map((project) => (
-                <Card key={project.slug}>
-                  <Article project={project} views={views[project.slug] ?? 0} />
-                </Card>
-              ))}
-          </div>
-          <div className="grid grid-cols-1 gap-4">
-            {sorted
-              .filter((_, i) => i % 3 === 1)
-              .map((project) => (
-                <Card key={project.slug}>
-                  <Article project={project} views={views[project.slug] ?? 0} />
-                </Card>
-              ))}
-          </div>
-          <div className="grid grid-cols-1 gap-4">
-            {sorted
-              .filter((_, i) => i % 3 === 2)
-              .map((project) => (
-                <Card key={project.slug}>
-                  <Article project={project} views={views[project.slug] ?? 0} />
-                </Card>
-              ))}
-          </div>
+        <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-2 lg:grid-cols-3">
+          {webProjects.map((project) => (
+            <Card key={project.slug}>
+              <Article project={project} views={views[project.slug] ?? 0} />
+            </Card>
+          ))}
         </div>
       </div>
     </div>
